f0a0108f2ab78f3c98657b9a7c3ec8472b5ae034
niormangari <juniormangari17@gmail>
2023-05-16 16:02:23 +0700

    second commit

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,13 +5,13 @@ const Contact = () => {
         <h3>Contact</h3>
       </div>
       <div className="link_contact">
-        <a href="https://www.instagram.com/juniormangari_" target="_blank">
+        <a href="https://www.instagram.com/juniormangari_">
           <img className="logo_contact" src="https://tse1.mm.bing.net/th?id=OIP.W3qszfgexOMKk9Nl_Owr4AHaHa&pid=Api&rs=1&c=1&qlt=95&w=122&h=122" alt="ig" />
         </a>
-        <a href="https://web.facebook.com/" target="_blank">
+        <a href="https://web.facebook.com/">
           <img className="logo_contact" src="https://tse2.mm.bing.net/th?id=OIP.55DCXbXlKDgEBoZhKxpzLAHaHa&pid=Api&P=0&h=180" alt="fb" />
         </a>
-        <a href="https://github.com/niormangari" target="_blank">
+        <a href="https://github.com/niormangari ">
           <img className="logo_contact" src="https://tse3.mm.bing.net/th?id=OIP.ckeUFk-yid0vfWnd56w7wAHaHa&pid=Api&P=0&h=180" alt="github" />
         </a>
       </div>
