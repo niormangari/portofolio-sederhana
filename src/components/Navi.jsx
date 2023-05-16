da28cf1e6849324b9ba89d4cd8aedfe66c8204a8
niormangari <juniormangari17@gmail>
2023-05-16 17:12:05 +0700

    perubahan di navbar

--- src/components/Navi.jsx
+++ src/components/Navi.jsx
@@ -1,7 +1,9 @@
+import React from "react";
+
 const Navi = () => {
   return (
     <>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <nav className="navbar navbar-expand-lg bg-body-tertiary ">
         <div className="container-fluid p-2 ">
           <a className=" text_brand navbar-brand navbar-dark ms-5" href="/">
             Weks.
@@ -24,21 +26,7 @@ const Navi = () => {
           </div>
         </div>
       </nav>
-      {/* <nav className="navbar navbar-expand-lg bg-body-tertiary ">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">Weks.</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse d-flex justify-content-end" id="navbarNavAltMarkup">
-      <div className="navbar-nav  ">
-        <a className="nav-link active" aria-current="page" href="#">Home</a>
-        <a className="nav-link" href="#">About</a>
-        <a className="nav-link" href="#">Contact</a>
-      </div>
-    </div>
-  </div>
-</nav> */}
+
       {/* <div className="navbar md-12 ">
         <ul className="list_navbar">
           <li className="">
