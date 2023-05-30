cf31dd80a9c950b7223dd594566cfb5ac1c613d0
niormangari <juniormangari17@gmail>
2023-05-30 14:21:46 +0700

    fix bug

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -7,6 +7,9 @@ const Contact = () => {
         <h3>Contact</h3>
       </div>
       <div className="link_contact">
+        <a href="mailto:niormangari@gmail.com">
+          <img className="logo_contact" src="https://tse1.mm.bing.net/th?id=OIP.DKnMNGB6q4y3Iqtnt7X5WAHaHa&pid=Api&P=0&h=180" alt="email" />
+        </a>
         <a href="https://www.instagram.com/juniormangari_">
           <img className="logo_contact" src="https://tse1.mm.bing.net/th?id=OIP.W3qszfgexOMKk9Nl_Owr4AHaHa&pid=Api&rs=1&c=1&qlt=95&w=122&h=122" alt="ig" />
         </a>
@@ -15,9 +18,6 @@ const Contact = () => {
         </a>
         <a href="https://github.com/niormangari ">
           <img className="logo_contact" src="https://tse3.mm.bing.net/th?id=OIP.ckeUFk-yid0vfWnd56w7wAHaHa&pid=Api&P=0&h=180" alt="github" />
-        </a>
-        <a href="mailto:niormangari@gmail.com">
-          <img className="logo_contact" src="https://tse1.mm.bing.net/th?id=OIP.DKnMNGB6q4y3Iqtnt7X5WAHaHa&pid=Api&P=0&h=180" alt="email" />
         </a>
       </div>
     </>
